30972d6ba98dac78d184a42036baa2a386388dd2
goddreject198 <goddreject198>
2023-06-01 19:58:28 +0700

    no message

---
+++ WebAppTest/Controllers/ClockController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class ClockController : Controller
+    {
+        // GET: Clock
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
