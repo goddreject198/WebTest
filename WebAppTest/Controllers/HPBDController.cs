fee3de0ec69f55e1e469af6da3851dd5f0a971f9
DESKTOP-PIRCBB2\Admin <goddreject198>
2023-06-04 13:45:53 +0700

    no message

---
+++ WebAppTest/Controllers/HPBDController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class HPBDController : Controller
+    {
+        // GET: HPBD
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
