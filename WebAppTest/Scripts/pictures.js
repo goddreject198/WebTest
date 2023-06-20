ed1f94653a998883a5bed1af8ff50ee1cafe68c5
goddreject198 <goddreject198>
2023-06-20 20:50:27 +0700

    no message

--- WebAppTest/Scripts/pictures.js
+++ WebAppTest/Scripts/pictures.js
@@ -5,8 +5,22 @@ images.forEach(image => {
         const img = image.querySelector('img');
         //alert(paragraph.innerText + ": " + img.src);
         //document.querySelector(".listPictures").classList.add("close");
-        document.querySelector(".picDetails").classList.add("active");
+        document.querySelector(".container").classList.add("active");
         document.querySelector(".listPictures").classList.add("unactive");
         document.querySelector(".picTag").src = img.src;
+        document.querySelector(".details").innerHTML = paragraph.innerText;
     });
 });
+
+document.querySelector("#closebtn").addEventListener("click", function () {
+    document.querySelector(".container").classList.remove("active");
+    document.querySelector(".listPictures").classList.remove("unactive");
+})
+
+document.querySelector("#prepic").addEventListener("click", function () {
+    alert("Pre pic");
+})
+
+document.querySelector("#nextpic").addEventListener("click", function () {
+    alert("Next pic");
+})
