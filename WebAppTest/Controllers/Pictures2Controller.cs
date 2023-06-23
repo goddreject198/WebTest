e544cbe213aed0aabbc3f3332fe9d4193694f66b
goddreject198 <goddreject198>
2023-06-23 20:55:09 +0700

    no message

--- WebAppTest/Controllers/Pictures2Controller.cs
+++ WebAppTest/Controllers/Pictures2Controller.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppTest.Models;
 
 namespace WebAppTest.Controllers
 {
-    public class ChessController : Controller
+    public class Pictures2Controller : Controller
     {
-        // GET: Chess
+        // GET: Pictures2
         public ActionResult Index()
         {
-            return View();
+            var list_pictures = new Pictures().Init().ToList();
+            return View(list_pictures);
         }
     }
 }
--- WebAppTest/Models/Pictures.cs
+++ WebAppTest/Models/Pictures.cs
@@ -15,14 +15,14 @@ namespace WebAppTest.Models
             List<Pictures> list_pic = new List<Pictures>();
             List<string> list = new List<string>()
             {
-                "https://unsplash.it/200/200"
-                , "https://unsplash.it/300/300"
-                , "https://unsplash.it/400/400"
-                , "https://unsplash.it/500/500"
-                , "https://unsplash.it/600/600"
-                , "https://unsplash.it/700/700"
-                , "https://unsplash.it/800/800"
-                , "https://unsplash.it/900/900"
+                "https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1517329782449-810562a4ec2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8OHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1613323593608-abc90fec84ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                , "https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
             };
             int i = 0;
             foreach (var pic in list)
