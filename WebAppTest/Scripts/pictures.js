2cb6a1f1d0121ea40dfcebb1ee1abc7fd93259fe
goddreject198 <goddreject198>
2023-06-28 19:43:08 +0700

    no message

--- WebAppTest/Scripts/pictures.js
+++ WebAppTest/Scripts/pictures.js
@@ -1,44 +1,87 @@
-﻿const images = document.querySelectorAll('.image');
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        const paragraph = image.querySelector('#image');
-        const img = image.querySelector('img');
-        const picid = image.querySelector('#picid');
-        //alert(paragraph.innerText + ": " + img.src);
-        //document.querySelector(".listPictures").classList.add("close");
-        document.querySelector(".container").classList.add("active");
-        document.querySelector(".listPictures").classList.add("unactive");
-        document.querySelector(".picTag").src = img.src;
-        document.querySelector(".details").innerHTML = paragraph.innerText;
-        document.querySelector("#piciddt").innerHTML = picid.innerText;
+﻿$(document).ready(function () {
+    $("#closebtn").click(function () {
+        $(".container").fadeOut();
+        $(".listPictures").fadeIn();
+    });
+    $("#prepic").click(function () { 
+        let current_id = Number(document.querySelector("#piciddt").innerHTML);
+        if (current_id == 1) {
+            return;
+        }
+        else {
+            document.querySelector(".picTag").src = images[current_id - 2].querySelector('img').src;
+            document.querySelector(".details").innerHTML = images[current_id - 2].querySelector('#image').innerText;
+            document.querySelector("#piciddt").innerHTML = current_id - 1;
+        }
+    });
+    $("#nextpic").click(function () {
+        let current_id = Number(document.querySelector("#piciddt").innerHTML);
+        if (current_id == 8) {
+            return;
+        }
+        else {
+            document.querySelector(".picTag").src = images[current_id].querySelector('img').src;
+            document.querySelector(".details").innerHTML = images[current_id].querySelector('#image').innerText;
+            document.querySelector("#piciddt").innerHTML = current_id + 1;
+        }
+    });
+    const images = document.querySelectorAll('.image');
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            const paragraph = image.querySelector('#image');
+            const img = image.querySelector('img');
+            const picid = image.querySelector('#picid');
+            $(".listPictures").fadeOut();
+            $(".container").css("display", "flex").hide().fadeIn("slow");
+            document.querySelector(".picTag").src = img.src;
+            $(".details").text($("#image").text());
+            $("#piciddt").text($("#picid").text());
+            debugger;
+        });
     });
 });
 
-document.querySelector("#closebtn").addEventListener("click", function () {
-    document.querySelector(".container").classList.remove("active");
-    document.querySelector(".listPictures").classList.remove("unactive");
-})
+//const images = document.querySelectorAll('.image');
+//images.forEach(image => {
+//    image.addEventListener('click', () => {
+//        const paragraph = image.querySelector('#image');
+//        const img = image.querySelector('img');
+//        const picid = image.querySelector('#picid');
+//        //alert(paragraph.innerText + ": " + img.src);
+//        //document.querySelector(".listPictures").classList.add("close");
+//        document.querySelector(".container").classList.add("active");
+//        document.querySelector(".listPictures").classList.add("unactive");
+//        document.querySelector(".picTag").src = img.src;
+//        document.querySelector(".details").innerHTML = paragraph.innerText;
+//        document.querySelector("#piciddt").innerHTML = picid.innerText;
+//    });
+//});
 
-document.querySelector("#prepic").addEventListener("click", function () {
-    let current_id = Number(document.querySelector("#piciddt").innerHTML);
-    if (current_id == 1) {
-        return;
-    }
-    else {
-        document.querySelector(".picTag").src = images[current_id - 2].querySelector('img').src;
-        document.querySelector(".details").innerHTML = images[current_id - 2].querySelector('#image').innerText;
-        document.querySelector("#piciddt").innerHTML = current_id - 1;
-    }
-})
+//document.querySelector("#closebtn").addEventListener("click", function () {
+//    document.querySelector(".container").classList.remove("active");
+//    document.querySelector(".listPictures").classList.remove("unactive");
+//})
 
-document.querySelector("#nextpic").addEventListener("click", function () {
-    let current_id = Number(document.querySelector("#piciddt").innerHTML);
-    if (current_id == 8) {
-        return;
-    }
-    else {
-        document.querySelector(".picTag").src = images[current_id].querySelector('img').src;
-        document.querySelector(".details").innerHTML = images[current_id].querySelector('#image').innerText;
-        document.querySelector("#piciddt").innerHTML = current_id + 1;
-    }
-})
+//document.querySelector("#prepic").addEventListener("click", function () {
+//    let current_id = Number(document.querySelector("#piciddt").innerHTML);
+//    if (current_id == 1) {
+//        return;
+//    }
+//    else {
+//        document.querySelector(".picTag").src = images[current_id - 2].querySelector('img').src;
+//        document.querySelector(".details").innerHTML = images[current_id - 2].querySelector('#image').innerText;
+//        document.querySelector("#piciddt").innerHTML = current_id - 1;
+//    }
+//})
+
+//document.querySelector("#nextpic").addEventListener("click", function () {
+//    let current_id = Number(document.querySelector("#piciddt").innerHTML);
+//    if (current_id == 8) {
+//        return;
+//    }
+//    else {
+//        document.querySelector(".picTag").src = images[current_id].querySelector('img').src;
+//        document.querySelector(".details").innerHTML = images[current_id].querySelector('#image').innerText;
+//        document.querySelector("#piciddt").innerHTML = current_id + 1;
+//    }
+//})
